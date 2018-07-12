7f2ed428f9c3f0c3d68163763388ffc21f1b9bcc
Joel Dubreuil <joel.dubreuil.qc@gmail.com>
2018-07-12 08:23:55 -0400

    Tutorial 2/10 24.54min

---
+++ Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+    }
+
+}
